94676fc8c29d85fb5045fdb9a304329f02b075cc
Alex Lee <hyunho98@gmail.com>
2020-04-13 00:18:47 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,6 +12,7 @@ class StudentsController < ApplicationController
   def activate
     student = Student.find(params[:id])
     student.active = !student.active
+    redirect_to student
   end
 
   private
